f4af95321d8ba349019593d57b81110cde656a8f
David Amend <David.Amend@dab.com>
2016-07-19 18:13:44 +0200

    fix to remove required svg sprites

--- tasks/styles/sass.js
+++ tasks/styles/sass.js
@@ -6,7 +6,7 @@ var CONFIG = CONFIG || require(pathToBuildConfig);
 var svgSrcFiles = CONFIG.SRC.SPRITES_IMG_BASE_FOLDER() + CONFIG.FILE_TYPE_MACHER.SVG();
 var plugins = plugins || {};
 
-var spritesTask = "./sprites";
+//var spritesTask = "./sprites";
 
 var compileSass = function (environment) {
     /*
@@ -47,10 +47,7 @@ var performCSS = function () {
 
     var myFilter = plugins.gulpFilter("**/*.css");
     //  To prevent async issues & writing to temp files, we need to write to memory stream
-    return plugins.gulpMerge(
-        require(spritesTask).generateSprites(svgSrcFiles),
-        compileSass()
-    )
+    return compileSass()
         .pipe(myFilter)
         // concat sprites.css with bootstrap.css
         .pipe(plugins.concat("css/main.css"))
